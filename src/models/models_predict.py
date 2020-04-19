c9b7a0e5ceb887ed992ef358691d4fb01db05e0a
Aurélien Massiot <amassiot@octo.com>
2020-04-19 09:50:21 +0200

    Add exponential weights with checkbox

--- src/models/models_predict.py
+++ src/models/models_predict.py
@@ -6,7 +6,6 @@ def get_prediction(sentence, vectorizer, classifiers, labels):
     x_test = vectorizer.transform(pd.Series(sentence))
     preds = np.zeros((x_test.shape[0], len(labels)))
     if not is_vectorized_sentence_empty(x_test):
-        print('ici')
         for idx, label in enumerate(labels):
             preds[:, idx] = classifiers[label].predict_proba(x_test)[:, 1]
     return preds, x_test
--- src/ui/streamlit_prediction.py
+++ src/ui/streamlit_prediction.py
@@ -1,8 +1,10 @@
 import glob
 import sys
 
+import numpy as np
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
 import streamlit as st
 
 sys.path.append('.')
@@ -18,6 +20,7 @@ st.title("Catégorisation d'une phrase toxique")
 classifiers, vectorizer = load_models(models_filenames, vectorizer_filename)
 
 input_sentence = st.text_input('Entrez une phrase:')
+st.markdown("La probabilité donnée par chaque régression logistique donne des pourcentages d'appartenance à chaque classe. Il y a possibilité d'afficher les odds pour mieux comprendre comment les poids font évoluer la prédiction.")
 preds, test_term_doc = get_prediction(input_sentence, vectorizer, classifiers, LABELS)
 preds_df = pd.DataFrame({'label': LABELS, 'preds': preds[0] * 100})
 fig = px.bar(preds_df,
@@ -40,16 +43,54 @@ chosen_label = st.sidebar.selectbox(
 )
 
 
-st.subheader('Interprétation de la phrase')
+st.subheader(f'Interprétation locale pour la catégorie {chosen_label}')
+st.markdown(f"Nous voyons ici au niveau d'une phrase quels mots ont le plus contribué à la prédiction dans la catégorie {chosen_label}. Pour cela, nous affichons les poids de la régression logistique ; il y a aussi la possibilité d'afficher leur exponentielle.")
 df_words_weight_toxic = get_local_weights_df(vectorizer, test_term_doc, classifiers, chosen_label)
-fig2 = px.bar(df_words_weight_toxic,
-              x='words',
-              y='weights')
+are_weights_exponential_local = st.checkbox('Afficher les poids exponentiels', key='are_weights_exponential_local')
+# fig2 = px.bar(df_words_weight_toxic,
+#               x='words',
+#               y='weights')
+fig2 = go.Figure()
+if not are_weights_exponential_local:
+    fig2.add_trace(go.Bar(
+        x=df_words_weight_toxic['words'],
+        y=df_words_weight_toxic['weights'],
+        name='weights',
+        marker_color='lightsalmon'
+    ))
+else:
+    fig2.add_trace(go.Bar(
+        x=df_words_weight_toxic['words'],
+        y=np.exp(df_words_weight_toxic['weights']),
+        name='exp(weights)',
+        marker_color='indianred'
+    ))
+
 words_weights_chart = st.plotly_chart(fig2)
 
-st.subheader('Interprétation globale (mots les plus $#@*)')
+st.subheader(f'Interprétation globale pour la catégorie {chosen_label}')
+st.markdown(f"Nous voyons au niveau du classifieur quels mots sont les plus impactants pour la classification dans la catégorie {chosen_label}. Il y a aussi la possibilité d'afficher leur exponentielle.")
 df_global_weights = get_global_weights_df(vectorizer, classifiers, chosen_label)
-fig3 = px.bar(df_global_weights.head(10),
-              x='words',
-              y='weights')
+are_weights_exponential_global = st.checkbox('Afficher les poids exponentiels', key='are_weights_exponential_global')
+# fig3 = px.bar(df_global_weights.head(10),
+#               x='words',
+#               y='weights',
+#               color=)
+fig3 = go.Figure()
+n_words = st.slider('Nombre de mots à afficher', min_value=5, max_value=20, value=5, step=1)
+df_global_weights_n_words = df_global_weights.head(n_words)
+if not are_weights_exponential_global:
+    fig3.add_trace(go.Bar(
+        x=df_global_weights_n_words['words'],
+        y=df_global_weights_n_words['weights'],
+        name='weights',
+        marker_color='lightsalmon'
+    ))
+else:
+    fig3.add_trace(go.Bar(
+        x=df_global_weights_n_words['words'],
+        y=np.exp(df_global_weights_n_words['weights']),
+        name='weights',
+        marker_color='indianred'
+    ))
 words_global_weights_chart = st.plotly_chart(fig3)
