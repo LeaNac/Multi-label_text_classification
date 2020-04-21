27430ca49c91e69d0fd73efc7f3b7593412a9c69
Aurélien Massiot <amassiot@octo.com>
2020-04-21 11:41:25 +0200

    Fix global variables

--- src/conf/global_variables.py
+++ src/conf/global_variables.py
@@ -5,9 +5,5 @@ DATA_PATH = PROJECT_ROOT / 'data'
 MODELS_PATH = PROJECT_ROOT / 'models'
 
 SEED = 42
-# LABELS = ['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']
-# TEXT_COLUMN = 'comment_text'
-
-
-LABELS = ['label_1', 'label_2']
-TEXT_COLUMN = 'text'
+LABELS = ['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']
+TEXT_COLUMN = 'comment_text'
